704757ac8d05037a9e8995a4b4d7a08e114234fe
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-12-05 17:32:24 -0500

    added button and function to click to other months

--- google-calendar-react/src/App.js
+++ google-calendar-react/src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import './App.css';
 import { getMonth } from './util';
 import CalendarHeader from './components/CalendarHeader';
 import Sidebar from './components/Sidebar';
 import Month from './components/Month';
+import GlobalContext from './context/GlobalContext';
 
 function App() {
-  const [currentMonth, setCurrentMonth] = useState(getMonth())
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const {monthIndex} = useContext(GlobalContext);
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
   
   return (
     <React.Fragment>
--- google-calendar-react/src/components/Day.js
+++ google-calendar-react/src/components/Day.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import dayjs from 'dayjs'
 
 export default function Day({day, rowIdx}) {
+  function getCurrentDayClass(){
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ? "bg-red-600 text-white rounded-full w-7" 
+    : "";
+  }
   return (
     <div className="border border-gray-200 flex flex-col">
        <header className="flex flex-col items-center">
@@ -9,7 +14,7 @@ export default function Day({day, rowIdx}) {
                      {day.format("ddd")}
                  </p>
                 )}
-            <p className="text-sm p-1 my-1 text-center">
+            <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
                 {day.format("DD")}
             </p>
        </header>
