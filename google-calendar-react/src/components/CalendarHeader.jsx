b9ed23c5209aa6cb0a6a6f60948b6717216f8550
keithprimas <136135449+keithprimas@users.noreply.github.com>
2023-12-05 22:21:28 -0500

    added smallcal components and updated other components

--- google-calendar-react/src/components/CalendarHeader.jsx
+++ google-calendar-react/src/components/CalendarHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import logo from '../assets/logo.png'
 import GlobalContext from '../context/GlobalContext'
+import dayjs from 'dayjs';
 
 export default function CalendarHeader() {
   const {monthIndex, setMonthIndex} = useContext(GlobalContext)
@@ -10,11 +11,14 @@ export default function CalendarHeader() {
   function handleNextMonth() {
     setMonthIndex(monthIndex + 1);
   }
+  function handleReset() {
+    setMonthIndex(dayjs().month());
+  }
   return (
     <header className="px-4 py-2 flex items-center">
         <img src={logo} alt="calendar" className="mr-2 w-12 h-12"/>
         <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
-        <button className="border rounded py-2 px-4 mr-5">
+        <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
             Today
         </button>
         <button onClick={handlePrevMonth}>
@@ -27,6 +31,11 @@ export default function CalendarHeader() {
               chevron_right
           </span>
         </button>
+        <h2 className="ml-4 text-xl text-gray-500 font-bold">
+          {dayjs(new Date(dayjs().year(), monthIndex)).format (
+            "MMMM YYYY"
+          )}
+        </h2>
     </header>
   )
 }
